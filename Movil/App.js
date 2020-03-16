f8273df19f57063c2fd57ab77aafb04cb9751460
Jorge Vargas <jorgevargasmateo@gmail.com>
2020-03-15 21:32:54 -0400

    Creando app movil

---
+++ Movil/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Welcome  from "./pages/Welcome/src/screens/Welcom.js";
+
+
+export default function App() {
+  return (
+    <View>
+     
+      <Welcome />
+    </View>
+  );
+}
+
---
+++ Movil/pages/newOrden/index.js
@@ -0,0 +1,1 @@
+export { default as Neworden } from "./src/screens/Neworden.js";
