0b0460cef978f1362ac05097a99de3de2e4e6c47
AnaOzuna01 <20175076@ce.pucmm.edu.do>
2020-03-22 19:58:18 -0400

    Agregando mas items a Inventario.jpx

--- web/src/components/ventas/verInventario.jsx
+++ web/src/components/ventas/verInventario.jsx
@@ -25,21 +25,119 @@ export class verInventario extends React.Component {
                 </div>
 
                 <div className="classCard">
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={productoImg} />
-                        <Card.Body className="form">
-                            <Card.Title>Hojas de colores</Card.Title>
-                            <Card.Text>
-                                Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
-                                saturación. Apto para cualquier tipo de impresión.
+
+
+                    <Row xs="2">
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={productoImg} />
+                                <Card.Body className="form">
+                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Text>
+                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
+                                        saturación. Apto para cualquier tipo de impresión.
+                                    </Card.Text>
+                                    <ButtonGroup toggle className="mb-2">
+                                        <ToggleButton type="checkbox" defaultChecked value="1">
+                                            Checked
+                                        </ToggleButton>
+                                    </ButtonGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={productoImg} />
+                                <Card.Body className="form">
+                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Text>
+                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
+                                        saturación. Apto para cualquier tipo de impresión.
                             </Card.Text>
-                            <ButtonGroup toggle className="mb-2">
-                                <ToggleButton type="checkbox" defaultChecked value="1">
-                                    Checked
+                                    <ButtonGroup toggle className="mb-2">
+                                        <ToggleButton type="checkbox" defaultChecked value="1">
+                                            Checked
                                 </ToggleButton>
-                            </ButtonGroup>
-                        </Card.Body>
-                    </Card>
+                                    </ButtonGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={productoImg} />
+                                <Card.Body className="form">
+                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Text>
+                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
+                                        saturación. Apto para cualquier tipo de impresión.
+                            </Card.Text>
+                                    <ButtonGroup toggle className="mb-2">
+                                        <ToggleButton type="checkbox" defaultChecked value="1">
+                                            Checked
+                                </ToggleButton>
+                                    </ButtonGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+
+                    <Row xs="2" className="classCard">
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={productoImg} />
+                                <Card.Body className="form">
+                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Text>
+                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
+                                        saturación. Apto para cualquier tipo de impresión.
+                            </Card.Text>
+                                    <ButtonGroup toggle className="mb-2">
+                                        <ToggleButton type="checkbox" defaultChecked value="1">
+                                            Checked
+                                </ToggleButton>
+                                    </ButtonGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={productoImg} />
+                                <Card.Body className="form">
+                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Text>
+                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
+                                        saturación. Apto para cualquier tipo de impresión.
+                            </Card.Text>
+                                    <ButtonGroup toggle className="mb-2">
+                                        <ToggleButton type="checkbox" defaultChecked value="1">
+                                            Checked
+                                </ToggleButton>
+                                    </ButtonGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+
+                        <Col>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={productoImg} />
+                                <Card.Body className="form">
+                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Text>
+                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
+                                        saturación. Apto para cualquier tipo de impresión.
+                            </Card.Text>
+                                    <ButtonGroup toggle className="mb-2">
+                                        <ToggleButton type="checkbox" defaultChecked value="1">
+                                            Checked
+                                </ToggleButton>
+                                    </ButtonGroup>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
                 </div>
             </div>
         );
