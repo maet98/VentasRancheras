d6cc8ff2713e8435e55bb8ed15c70e6fc388be82
AnaOzuna01 <20175076@ce.pucmm.edu.do>
2020-03-22 20:16:32 -0400

    Editando productos a mostrar en verInventario.jsx

--- web/src/components/ventas/verInventario.jsx
+++ web/src/components/ventas/verInventario.jsx
@@ -39,7 +39,7 @@ export class verInventario extends React.Component {
                                     </Card.Text>
                                     <ButtonGroup toggle className="mb-2">
                                         <ToggleButton type="checkbox" defaultChecked value="1">
-                                            Checked
+                                        Ver producto
                                         </ToggleButton>
                                     </ButtonGroup>
                                 </Card.Body>
@@ -50,14 +50,14 @@ export class verInventario extends React.Component {
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src={productoImg} />
                                 <Card.Body className="form">
-                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Title>Calculadora</Card.Title>
                                     <Card.Text>
-                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
-                                        saturación. Apto para cualquier tipo de impresión.
+                                        Fuente de alimentación dual por energía solar o 1 pila AA.
+                                        Apagado automático, ahorra más energía (botón ON/OFF).
                             </Card.Text>
                                     <ButtonGroup toggle className="mb-2">
                                         <ToggleButton type="checkbox" defaultChecked value="1">
-                                            Checked
+                                        Ver producto
                                 </ToggleButton>
                                     </ButtonGroup>
                                 </Card.Body>
@@ -68,14 +68,14 @@ export class verInventario extends React.Component {
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src={productoImg} />
                                 <Card.Body className="form">
-                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Title>Mouse Pad</Card.Title>
                                     <Card.Text>
-                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
-                                        saturación. Apto para cualquier tipo de impresión.
+                                    Minialfombrilla de ratón para videojuegos: ideal para los 
+                                    videojuegos, diseñadores gráficos o cualquiera que use un ratón.
                             </Card.Text>
                                     <ButtonGroup toggle className="mb-2">
                                         <ToggleButton type="checkbox" defaultChecked value="1">
-                                            Checked
+                                        Ver producto
                                 </ToggleButton>
                                     </ButtonGroup>
                                 </Card.Body>
@@ -88,14 +88,14 @@ export class verInventario extends React.Component {
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src={productoImg} />
                                 <Card.Body className="form">
-                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Title>Escritorio</Card.Title>
                                     <Card.Text>
-                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
-                                        saturación. Apto para cualquier tipo de impresión.
+                                        Tablero aglomerado de 16 mm de estructura resistente
+                                        Diseño elegante y a la vez completamente práctico.
                             </Card.Text>
                                     <ButtonGroup toggle className="mb-2">
                                         <ToggleButton type="checkbox" defaultChecked value="1">
-                                            Checked
+                                        Ver producto
                                 </ToggleButton>
                                     </ButtonGroup>
                                 </Card.Body>
@@ -106,14 +106,14 @@ export class verInventario extends React.Component {
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src={productoImg} />
                                 <Card.Body className="form">
-                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Title>Lámpara</Card.Title>
                                     <Card.Text>
-                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
-                                        saturación. Apto para cualquier tipo de impresión.
+                                    Esta lámpara lupa es ajustable y giratorio según su necesidad, muy práctica. 
+                                    El diseño de manos libres, con la lente de aumento 8x.
                             </Card.Text>
                                     <ButtonGroup toggle className="mb-2">
                                         <ToggleButton type="checkbox" defaultChecked value="1">
-                                            Checked
+                                        Ver producto
                                 </ToggleButton>
                                     </ButtonGroup>
                                 </Card.Body>
@@ -124,14 +124,14 @@ export class verInventario extends React.Component {
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src={productoImg} />
                                 <Card.Body className="form">
-                                    <Card.Title>Hojas de colores</Card.Title>
+                                    <Card.Title>Silla Giratoria</Card.Title>
                                     <Card.Text>
-                                        Amplia gama de papel de colores. Papel de gran calidad y colores de gran intensidad y
-                                        saturación. Apto para cualquier tipo de impresión.
+                                    Asiento: 35 cm. Altura total: 75–87 cm aprox.  
+                                    Regulable en altura, giro completo de 360°. Asiento acolchado, fácil de limpiar.
                             </Card.Text>
                                     <ButtonGroup toggle className="mb-2">
                                         <ToggleButton type="checkbox" defaultChecked value="1">
-                                            Checked
+                                            Ver producto
                                 </ToggleButton>
                                     </ButtonGroup>
                                 </Card.Body>
