fc4a1fba82cfd8abd187d8a819ba904ebf21b595
miguel <maletaveras@gmail.com>
2020-04-05 20:37:58 -0400

    Cambio de compania

--- api/server/src/employee/controller.js
+++ api/server/src/employee/controller.js
@@ -1,66 +1,95 @@
 const QBO = require("../../ERP/index");
-import {employee} from "../models/employee";
+import {
+    employee
+} from "../models/employee";
 import config from "config";
-import bcrypt, { hashSync, compareSync } from "bcrypt";
-import { employeeCustomer } from "../models/employee-customer";
+import bcrypt, {
+    hashSync,
+    compareSync
+} from "bcrypt";
+import {
+    employeeCustomer
+} from "../models/employee-customer";
 import jwt from "jsonwebtoken";
 
 export async function getAll(req, res) {
     const qbo = await QBO.getQbo();
-    qbo.findEmployees({fetchAll: true},(err,ans) =>{
-        if(err){
+    qbo.findEmployees({
+        fetchAll: true
+    }, (err, ans) => {
+        if (err) {
             res.status(400).json(err);
-        }
-        else{
+        } else {
             res.json(ans.QueryResponse);
         }
     });
 }
 
-export async function filterName(req,res){
+export async function filterName(req, res) {
     const qbo = await QBO.getQbo();
     var name = req.params.name;
-    name = '%'+name+'%';
-    qbo.findEmployees([
-    {field:'fetchAll',value: true},
-    {field:'DisplayName',value:name,operator:'LIKE'}
-    ],(err,employees) =>{
-        if(err){
-            return res.status(400).json(err);
+    name = "%" + name + "%";
+    qbo.findEmployees(
+        [{
+                field: "fetchAll",
+                value: true
+            },
+            {
+                field: "DisplayName",
+                value: name,
+                operator: "LIKE"
+            },
+        ],
+        (err, employees) => {
+            if (err) {
+                return res.status(400).json(err);
+            }
+            res.json(employees.QueryResponse);
         }
-        res.json(employees.QueryResponse);
-    })
+    );
 }
 
-export async function getById(req, res){
+export async function getById(req, res) {
+    const qbo = await QBO.getQbo();
     console.log(req.params.Id);
-    employee.findAll({
-        where:{
-            employeeId: req.params.Id
-        },
-        limit: 1
-    })
-    .then(ans =>{
-        if(ans.length === 0){
-            res.json({"msg": "There is not Employee with that id: "+req.params.Id})
-        }
-        else{
-            res.json(ans[0]);
-        }
-    })
-    .catch(err =>{
-        res.status(400).json(err);
-    })
+    employee
+        .findAll({
+            where: {
+                employeeId: req.params.Id,
+            },
+            limit: 1,
+        })
+        .then((ans) => {
+            if (ans.length === 0) {
+                res.json({
+                    msg: "There is not Employee with that id: " + req.params.Id
+                });
+            } else {
+                qbo.getEmployee(req.params.Id, (err, obj) => {
+                    if (err) {
+                        return res.status(400).json(err);
+                    }
+                    console.log();
+                    res.json({
+                        ...ans[0].dataValues,
+                        ...obj
+                    });
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
 }
 
 export async function createEmployee(req, res) {
     const qbo = await QBO.getQbo();
     const newEmployee = {
-        GivenName: req.body.firstName +" "+req.body.lastName,
+        GivenName: req.body.firstName + " " + req.body.lastName,
         FamilyName: req.body.lastName,
-    }
-    qbo.createEmployee(newEmployee,(err,ans) =>{
-        if(err){
+    };
+    qbo.createEmployee(newEmployee, (err, ans) => {
+        if (err) {
             return res.status(400).json(err);
         }
         const dbEmployee = {
@@ -68,79 +97,90 @@ export async function createEmployee(req, res) {
             username: req.body.username,
             email: req.body.email,
             password: req.body.password,
-            type: req.body.type
+            type: req.body.type,
         };
-        employee.create(dbEmployee)
-        .then(resp =>{
-            res.json(resp);
-        })
-        .catch(err =>{
-            qbo.deleteEmployee(ans.Id,(err,ans) =>{
-                if(err){
-
-                }
-                else{
-                    res.status(400).json(err);
-                }
+        employee
+            .create(dbEmployee)
+            .then((resp) => {
+                res.json(resp);
             })
-        })
-    })
-}
-
-export function login(req, res) {
-    const {email, password} = req.body;
-    employee.findAll({where:{email: email}})
-    .then(async (ans) => {
-        if(compareSync(password,ans[0].password)){
-            console.log(ans[0].employeeId)
-            const qbo = await QBO.getQbo();
-            qbo.getEmployee(ans[0].employeeId.toString(),(err,customer)=>{
-                if(err){
-                    return res.status(400).json(err);
-                }
-                customer.type = ans[0].type;
-                customer.email = ans[0].email;
-                res.json(customer);
-            })
-        }
-        else{
-            res.status(400).json({ok:false})
-        }
-    })
-    .catch(err =>{
-        res.status(400).json({msg:`User not found with that email: ${email}`});
+            .catch((err) => {
+                qbo.deleteEmployee(ans.Id, (err, ans) => {
+                    if (err) {} else {
+                        res.status(400).json(err);
+                    }
+                });
+            });
     });
 }
 
+export function login(req, res) {
+    const {
+        email,
+        password
+    } = req.body;
+    employee
+        .findAll({
+            where: {
+                email: email
+            }
+        })
+        .then(async (ans) => {
+            if (compareSync(password, ans[0].password)) {
+                console.log(ans[0].employeeId);
+                const qbo = await QBO.getQbo();
+                qbo.getEmployee(ans[0].employeeId.toString(), (err, customer) => {
+                    if (err) {
+                        return res.status(400).json(err);
+                    }
+                    customer.type = ans[0].type;
+                    customer.email = ans[0].email;
+                    res.json(customer);
+                });
+            } else {
+                res.status(400).json({
+                    ok: false
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(400).json({
+                msg: `User not found with that email: ${email}`
+            });
+        });
+}
 
-export async function getCustomer(req, res){
+export async function getCustomer(req, res) {
     const id = req.params.id;
     const qbo = await QBO.getQbo();
-    qbo.findCustomers({fetchAll: true},(err,ans) =>{
-        if(err){
+    qbo.findCustomers({
+        fetchAll: true
+    }, (err, ans) => {
+        if (err) {
             return res.status(400).json(err);
         }
         var customers = ans.QueryResponse.Customer;
-        var arr =[]
-        employeeCustomer.findAll({
-            where:{
-                employeeId: id
-            }
-        })
-        .then(employee =>{
-            for(let i = 0;i < customers.length;i++){
-                let can = false;
-                for(let j = 0;j < employee.length;j++){
-                    if(employee[j].dataValues.CustomerId == customers[i].Id){
-                        can = true;
-                        break;
+        var arr = [];
+        employeeCustomer
+            .findAll({
+                where: {
+                    employeeId: id,
+                },
+            })
+            .then((employee) => {
+                for (let i = 0; i < customers.length; i++) {
+                    let can = false;
+                    for (let j = 0; j < employee.length; j++) {
+                        if (employee[j].dataValues.CustomerId == customers[i].Id) {
+                            can = true;
+                            break;
+                        }
+                    }
+                    if (can == true) {
+                        arr.push(customers[i]);
                     }
                 }
-                if(can == true){
-                    arr.push(customers[i])
-                }
-            }
-            return res.json(arr);  
-        })
-    })
+                return res.json(arr);
+            });
+    });
 }
